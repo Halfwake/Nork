75fef80cc56bda1a82e269a3e32cddc25018cc67
Drew Dudash <drewdudash@hotmail.com>
2012-05-30 11:01:23 -0500

    Added batch support for camera, and implemented basic rendering.

--- map_maker.py
+++ map_maker.py
@@ -1,7 +1,66 @@
 import pyglet
 import tile
-import pickle
+import menu
 
-f_file = open("TEST_MAP", "wb", pickle.HIGHEST_PROTOCOL)
-instance = tile.Map(20, 20)
-pickle.dump(instance, f_file)
+IMAGES = {"save" : pyglet.image.load("resources/art/menu_save.png"),
+          "load" : pyglet.image.load("resources/art/menu_load.png"),
+          "new" : pyglet.image.load("resources/art/menu_new.png")}
+
+while True:
+    reply = raw_input("Load file? y/n")
+    if reply.upper() == "Y":
+        reply = raw_input("What is the file name?")
+        try:
+            f_working = reply
+            obj_working = tile.load_file(f_working)
+            break
+        except IOError:
+            print "File not found."
+        break
+    if reply.upper() == "N":
+        reply = raw_input("What is the new file name?")
+        try:
+            f_working = reply
+            width = int(raw_input("Width?"))
+            height = int(raw_input("Height?"))
+            obj_working = tile.Map(width, height)
+            check = open(f_working)
+            check.close()
+            break
+        except IOError:
+            "Invalid name."
+        break
+    print "Not a proper anwser."
+
+class Interface(pyglet.window.Window):
+    def __init__(self):
+        super(Interface, self).__init__()
+        self.camera = tile.Camera(obj_working, 10, 10)
+        self.fps_display = pyglet.clock.ClockDisplay(color = (200.0, 200.0, 200.5, 128.0))
+        pyglet.clock.set_fps_limit(40)
+        self.save_button = menu.Button(0, 0, IMAGES["save"], self.save_file)
+        self.load_button = menu.Button(self.save_button.x + self.save_button.width, 0, IMAGES["load"], self.load_file)
+        self.new_button = menu.Button(self.load_button.x + self.load_button.width, 0, IMAGES["new"], self.new_file)
+    def save_file(self):
+        tile.save_file(f_working, obj_working)
+    def load_file(self):
+        try:
+            reply = raw_input("What file would you like to load? ")
+            obj_working = tile.load_file(reply)
+        except IOError:
+            print "File not found."
+    def new_file(self):
+        obj_working = tile.Map(10, 10)
+    def on_mouse_press(self, x, y, symbol, modifiers):
+        self.save_button.click(x, y)
+        self.load_button.click(x, y)
+        self.new_button.click(x, y)
+    def on_draw(self):
+        self.clear()
+        self.camera.draw()
+        self.save_button.draw()
+        self.load_button.draw()
+        self.new_button.draw()
+        self.fps_display.draw()
+root = Interface()
+pyglet.app.run()
--- tile.py
+++ tile.py
@@ -3,45 +3,84 @@ import pyglet
 IMAGES = {"tile" : pyglet.image.ImageGrid(pyglet.image.load("resources/art/tile.png"), 8, 8)}
 IMAGES["grass1"] = IMAGES["tile"][56]
 
-
 class Tile(pyglet.sprite.Sprite):
     def __init__(self, image, blocked):
         super(Tile, self).__init__(image)
         self.blocked = blocked
-
+TILES = {}
 class GrassTile(Tile):
     def __init__(self):
         super(GrassTile, self).__init__(IMAGES["grass1"], False)
+        self.hash_name = "grass"
+TILES["grass"] = GrassTile
+
 
 class Map(object):
+    "Includes an entire map file in it, can be serialized with load_file and save_file functions."
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.content = [[GrassTile() for x in range(self.width)] for y in range(self.height)]
-    def camera_grab(self, x, y, width, height):
-        final = []
-        for row in range(y, height):
-            final.append(self.content[row][x:width])
-        return final
+        self.content = [[TILES["grass"]() for x in range(self.width)] for y in range(self.height)]
 class Camera(object):
+    "Camera isntances contain a visible area and can be drawn."
     def __init__(self, a_map, width, height):
         self.a_map = a_map
         self.width = width
         self.height = height
-        self.lock = self.a_map.camera_grab(0, 0, self.width, self.height)
+        self.batch = pyglet.graphics.Batch()
+        self.camera_grab(0, 0, self.width, self.height)
     def camera_grab(self, x, y, width, height):
-        self.lock = self.a_map.camera_grab(x, y, width, height)
-    def draw(self, x, y):
-        sprite = self.lock[0][0]
-        for y,row in enumerate(self.lock):
-            for x,tile in enumerate(row):
-                tile.x = x
-                tile.y = y
-                tile.draw()
-def load_file():
-    pass
-def save_file():
-    pass
+        "Changes the camera's view of the map."
+        final = [] #Sets the starting variable
+        for row in final: #Clears the old locks batch references
+            for item in row:
+                item.batch = None()
+        for row in range(y, y + height): #Grabs the new part of the map
+            final.append(self.a_map.content[row][x:x + width])
+        self.lock = final #Changes the lock
+        for y,row in enumerate(self.lock): #Positions the tiles in lock and gives them a batch
+            for x,item in enumerate(row):
+                item.x = x * item.width
+                item.y = y * item.height
+                item.batch = self.batch
+    def draw(self):
+        self.batch.draw()
+def load_file(file_name):
+    "Loads map files"
+    f_map = open(file_name, "rb")
+    try:
+        width = int(f_map.readline())
+        height = int(f_map.readline())
+        new_map = Map(width, height)
+        x, y = 0, 0
+        for line in f_map.readlines():
+            if line == "\n":
+                y += 1
+                x = 0
+            else:
+                new_map.content[x][y] = TILES[line[:-1]]()
+                x += 1
+        return new_map
+            
+    finally:
+        f_map.close()
+def save_file(file_name, a_map):
+    "Saves map files to 'file_name'"
+    f_map = open(file_name, "wb")
+    try:
+        f_map.write(str(a_map.width) + "\n")
+        f_map.write(str(a_map.height) + "\n")
+        for row in a_map.content:
+            for element in row:
+                f_map.write(element.hash_name + "\n")
+            f_map.write("\n")
+    finally:
+        f_map.close()
+        
 if __name__ == "__main__":
-    pass
+    a_map = Map(10,10)
+    save_file("test.txt", a_map)
+    test = load_file("test.txt")
+    x = Camera(test, 10, 10)
+    print test.height
     
